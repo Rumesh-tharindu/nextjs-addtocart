90479fc577dfffa5ab6c03798b6127274ff34b20
rumesh20 <rumesh@saberion.lk>
2023-09-05 11:20:35 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,13 @@ export default function Home() {
             <CategoryCard image="https://imgur.com/3Y1DLYC.png" name="PS5" />
             <CategoryCard image="https://imgur.com/Dm212HS.png" name="Switch" />
           </div>
+          <div className="largeGrid">
+            <CategoryCard image="https://imgur.com/qb6IW1f.png" name="PC" />
+            <CategoryCard
+              image="https://imgur.com/HsUfuRU.png"
+              name="Accessories"
+            />
+          </div>
         </div>
       </section>
     </div>
